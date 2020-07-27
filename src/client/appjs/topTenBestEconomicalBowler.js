2daa6a563f8df9a0a3771c6fb25c34f25eb3da0b
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-07-27 11:59:47 +0530

    feat: implements function to visulaise data of top ten economical bowler

---
+++ src/client/appjs/topTenBestEconomicalBowler.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded",async  () => {
+    let topTenBestEconomicalBowler = await fetch("http://localhost:3000/topTenBestBowlerByEconomy", {
+      "Content-Type": "text/json",
+      "Access-Control-Allow-Origin": "*",
+    })
+    topTenBestEconomicalBowler = await topTenBestEconomicalBowler.json();
+    
+    const hightChartData = topTenBestEconomicalBowler.map(key => {
+      return {
+        name: key.bowler,
+        y: key.economy,
+      }
+    })
+    
+    console.log(hightChartData)
+    
+      Highcharts.chart("container3", {
+        chart: {
+          type: "column",
+        },
+        title: {
+          text: "Top ten Economical Bowler in 2015",
+        },
+      
+        accessibility: {
+          announceNewData: {
+            enabled: true,
+          },
+        },
+        xAxis: {
+          type: "category",
+        },
+        yAxis: {
+          title: {
+            text: "Bowler Economy",
+          },
+        },
+        legend: {
+          enabled: false,
+        },
+        plotOptions: {
+          series: {
+            borderWidth: 0,
+            dataLabels: {
+              enabled: true,
+              format: "{point.y:.2f}",
+            },
+          },
+        },
+    
+        tooltip: {
+          headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+          pointFormat:
+            '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b><br/>',
+        },
+    
+        series: [
+          {
+            name: "Bowler Name with Economy",
+            colorByPoint: true,
+            data: hightChartData
+          },
+        ]
+      });
+    }
+    
+    );
+    

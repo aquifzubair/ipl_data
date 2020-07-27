b167fc78fcb5a63feb4a12f56b73f4791732cd3f
aquifzubair2 <aquif.zunair@mountblue.tech>
2020-07-27 11:26:21 +0530

    feat: implements a server to fetch outputs json file using http module

---
+++ src/server/app.js
@@ -0,0 +1,53 @@
+var http = require("http");
+var fs = require("fs");
+var port = "3000";
+
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, {
+    "Content-Type": "text/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+
+  const readGivenFile = (path) => {
+    fs.readFile(path, function (err, content) {
+      if (err) {
+        response.writeHead(400, {
+          "Content-Type": "text/json",
+          "Access-Control-Allow-Origin": "*",
+        });
+        response.write("File not found");
+      } else {
+        response.write(content);
+        response.end();
+      }
+    });
+  };
+  switch (request.url) {
+    case "/matchesPerYear": {
+      readGivenFile("./../output/matchesPerYear.json");
+      break;
+    }
+    case "/extraRunPerTeamIn2016": {
+      readGivenFile("./../output/extraRunPerTeamIn2016.json");
+      break;
+    }
+    case "/wonMatchesPerYear": {
+      readGivenFile("./../output/wonMatchesPerYear.json");
+      break;
+    }
+    case "/topTenBestBowlerByEconomy": {
+      readGivenFile("./../output/topTenBestBowlerByEconomy.json");
+      break;
+    }
+    default: {
+      response.writeHead(404, {
+        "Content-Type": "text/json",
+        "Access-Control-Allow-Origin": "*",
+      });
+      response.end();
+    }
+  }
+});
+
+server.listen(port);
+console.log("server is listening at port " + port);
